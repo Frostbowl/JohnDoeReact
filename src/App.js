20b7c4c75d9a51317d5156f6318b976d39bbacc2
Frostbowl <masclet.c@gmail.com>
2024-03-24 21:37:06 +0400

    end footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import {Route, Routes, Link} from 'react-router-dom';
 import React from 'react';
 import Nav from './pages/header/Nav';
 import Home from './pages/main/Home';
+import Footer from './pages/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
         <Routes>
-          <Route path='/' element= {<Home/>}></Route>
+          <Route path='/' element= {<Home/>}/>
         </Routes>
+      <Footer/>
+
     </div>
   );
 }
