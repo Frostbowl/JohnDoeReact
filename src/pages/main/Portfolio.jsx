5d08992d8ec4fdd493d34c69f0faf1f8fd0c81b9
Frostbowl <masclet.c@gmail.com>
2024-03-26 11:54:53 +0400

    end Realisation

--- src/pages/main/Portfolio.jsx
+++ src/pages/main/Portfolio.jsx
@@ -8,50 +8,59 @@ const Portfolio = () =>{
         <main>
             <div className='portfolioBg'>
             </div>
-            <div className='titrePortfolio text-center'>
+            <div className='titrePortfolio text-center '>
                 <h1>Mes créations</h1>
                 <h2>Voici quelques unes de mes créations</h2>
             </div>
 
-            <div className='container text-center creations'>
-
-                <div class="card " style={{width: "18rem"}}>
-                    <img src='fresh-food.jpg' alt="" className='card-img-top' />
-                    <div class="card-body">
-                        <h4 class="card-title">Freh food</h4>
-                        <p class="card-text">Réalisation d'un site avec commande en ligne.</p>
-                        <a href="#" class="btn btn-outline-primary">voir</a>
-                     </div>
-                    <div>
-                        <p className='card-footer'>Site réalisé avec PHP et MySQL</p>
-                    </div>
-                </div>
-
-                <div class="card" style={{width: "18rem"}}>
-                    <img src='../../../public/img/portfolio/espace-bien-etre.jpg' alt="" />
-                    <div class="card-body">
-                        <h4 class="card-title">Restaurant Akira</h4>
-                        <p class="card-text">Réalisation d'un site vitrine</p>
-                        <a href="#" class="btn btn-outline-primary">voir</a>
-                     </div>
-                    <div>
-                        <p className='card-footer'>Site réalisé avec WordPress</p>
-                    </div> 
-                </div>
+            <div className='container text-center'>
+                <div class="row  ">
                 
-                <div class="card " style={{width: "18rem"}}>
-                    <img src='../../../public/img/portfolio/espace-bien-etre.jpg' alt="" />
-                    <div class="card-body ">
-                        <h4 class="card-title">Espace bien-être</h4>
-                        <p class="card-text">Réalisation d'un site vitrine pour un praticien de bien-être</p>
-                        <a href="#" class="btn btn-outline-primary">voir</a>
-                     </div>
-                    <div>
-                        <p className='card-footer'>Site réalisé en HTML/CSS</p>
+                    <div class="col-md-5 col-xl-4 p-2">
+                        <div class="card" >
+                            <img src="..//img/portfolio/fresh-food.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Fresh food</h5>
+                                <p class="card-text">Réalisation d'un site avec commande en ligne.</p>
+                                <a href="#" class="btn btn-outline-primary">voir</a>
+                            </div>
+                            <div>
+                                <p className='card-footer m-0'>Site réalisé en HTML/CSS</p>
+                            </div>
+                        </div>
                     </div>
-                </div> 
-            </div>
 
+                    <div class="col-md-5 col-xl-4 p-2">
+                        <div class="card">
+                            <img src="../img/portfolio/restaurant-japonais.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Restaurant Akira</h5>
+                                <p class="card-text">Réalisation d'un site vitrine</p>
+                                <a href="#" class="btn btn-outline-primary">voir</a>
+                            </div>
+                            <div>
+                                <p className='card-footer m-0'>Site réalisé en HTML/CSS</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-5 col-xl-4 p-2">
+                        <div class="card">
+                            <img src="../img/portfolio/espace-bien-etre.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Espace bien-être</h5>
+                                <p class="card-text">Réalisation d'un site vitrine pour un praticien de bien-être</p>
+                                <a href="#" class="btn btn-outline-primary">voir</a>
+                            </div>
+                            <div>
+                                <p className='card-footer m-0'>Site réalisé en HTML/CSS</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
 
         </main>
     )
